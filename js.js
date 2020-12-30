ac20cf44eba305047fa690fbe851fe408fbea38c
Clyde <56137888+Clyde-Beep@users.noreply.github.com>
2020-12-30 03:06:39 -0500

    Create js.js

---
+++ js.js
@@ -0,0 +1,33 @@
+var i = 0;
+
+var myVar = setInterval(myTimer, 3);
+
+function myTimer() {
+
+    var els = document.getElementById("grid-container").getElementsByClassName("ytd-expanded-shelf-contents-renderer");
+
+    if (i < els.length) {
+
+        els[i].querySelector("[aria-label^='Unsubscribe from']").click();
+
+        setTimeout(function() {
+
+            var unSubBtn = document.getElementById("confirm-button").click();
+
+        }, 2);
+
+        setTimeout(function() {
+
+            els[i].parentNode.removeChild(els[i]);
+
+        }, 2);
+
+    }
+
+    i++;
+
+    console.log(i + " unsubscribing process completed!");
+
+    console.log(els.length + " remaining");
+
+}
